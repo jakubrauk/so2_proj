e7bef2bc2a29cfd8c689d6a3573893d7f1b8ced9
jacob <kubarauk@gmail.com>
2023-06-06 23:58:11 +0200

    barely working init comit

--- ball.h
+++ ball.h
@@ -12,9 +12,13 @@ class Ball {
         int max_bounces = 6;
         bool exists = true;
         char ball_ch;
+        bool is_blocking;
 
         Ball(int start_x, int start_y, int max_bounces_, int initial_speed, bool *run_);
-
+        
+        int get_next_y();
+        int get_zone_number();
+        int get_next_zone_number();
         char random_ball_char();
         void move_ball();
         void thread_func();
